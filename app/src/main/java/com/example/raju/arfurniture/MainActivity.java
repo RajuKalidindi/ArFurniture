66073b78507d39d4741ae4c036921a53c01b61df
RajuKalidindi <krkraju997@gmail.com>
2018-09-17 01:25:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/raju/arfurniture/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.raju.arfurniture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.ar.sceneform.ux.ArFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+    }
+}
